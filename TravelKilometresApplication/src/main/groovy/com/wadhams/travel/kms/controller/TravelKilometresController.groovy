2fc83b59881eda92e5ef766f9acacee8d1fc962a
robert.wadhams <robert.wadhams@gmail.com>
2021-08-28 08:21:53 +1000

    Add FuelEconomy reporting to TravelKilometresApp

--- TravelKilometresApplication/src/main/groovy/com/wadhams/travel/kms/controller/TravelKilometresController.groovy
+++ TravelKilometresApplication/src/main/groovy/com/wadhams/travel/kms/controller/TravelKilometresController.groovy
@@ -3,12 +3,15 @@ package com.wadhams.travel.kms.controller
 import java.math.BigDecimal
 
 import com.wadhams.travel.kms.dto.FuelDTO
+import com.wadhams.travel.kms.dto.FuelEconomyDTO
 import com.wadhams.travel.kms.dto.ServiceDTO
 import com.wadhams.travel.kms.dto.TravelDTO
 import com.wadhams.travel.kms.report.FuelDetailReportService
+import com.wadhams.travel.kms.report.FuelEconomyReportService
 import com.wadhams.travel.kms.report.ServiceReportService
 import com.wadhams.travel.kms.report.TravelReportService
 import com.wadhams.travel.kms.service.TravelListService
+import com.wadhams.travel.kms.service.FuelEconomyService
 import com.wadhams.travel.kms.service.FuelXMLService
 import com.wadhams.travel.kms.service.ServiceXMLService
 import com.wadhams.travel.kms.service.TravelXMLService
@@ -37,5 +40,14 @@ class TravelKilometresController {
 		
 		ServiceReportService service = new ServiceReportService()
 		service.execute(serviceDTO, totalCaravanKms, carOdometerKms)
+		
+		FuelEconomyService feService = new FuelEconomyService()
+		List<FuelEconomyDTO> feList = feService.buildFuelEconomyList(fuelList)
+		feService.addCaravanTripsToFuelEconomyList(feList, travelList)
+		feService.calculateAdditionalValues(feList)
+		
+		FuelEconomyReportService ferService = new FuelEconomyReportService()
+		ferService.reportByDate(feList)
+		ferService.reportByPerformance(feList)
 	}
 }

19837bd130d8c4b85759ee4b6af98cf470dac16f
robert.wadhams <robert.wadhams@gmail.com>
2024-02-28 16:45:55 +1100

    Cleanup of TempController

--- TravelKilometresApplication/src/test/groovy/com/wadhams/travel/kms/controller/TempController.groovy
+++ TravelKilometresApplication/src/test/groovy/com/wadhams/travel/kms/controller/TempController.groovy
@@ -1,36 +1,42 @@
 package com.wadhams.travel.kms.controller
 
-import com.wadhams.travel.kms.dto.FuelDTO
-import com.wadhams.travel.kms.dto.TravelDTO
-import com.wadhams.travel.kms.dto.TripDTO
-import com.wadhams.travel.kms.report.TripReportService
-import com.wadhams.travel.kms.service.FuelXMLService
-import com.wadhams.travel.kms.service.TravelXMLService
-import com.wadhams.travel.kms.service.TripListService
-import com.wadhams.travel.kms.service.TripXMLService
+import com.wadhams.travel.kms.dto.ServiceContainerDTO
+import com.wadhams.travel.kms.service.ServiceXMLService
 
 class TempController {
 	def execute() {
-		FuelXMLService fuelXMLService = new FuelXMLService()
-		List<FuelDTO> fuelList = fuelXMLService.loadFuelData()
+//		FuelXMLService fuelXMLService = new FuelXMLService()
+//		List<FuelDTO> fuelList = fuelXMLService.loadFuelData()
+//		fuelList.each {f ->
+//			println f
+//		}
 
-		TravelXMLService travelXMLService = new TravelXMLService()
-		List<TravelDTO> travelList = travelXMLService.loadTravelData()
-
-		TripXMLService tripXMLService = new TripXMLService()
-		List<TripDTO> tripList = tripXMLService.loadTripData()
+//		TravelXMLService travelXMLService = new TravelXMLService()
+//		List<TravelDTO> travelList = travelXMLService.loadTravelData()
+//		travelList.each {t ->
+//			println t
+//		}
+		
+//		ServiceXMLService serviceXMLService = new ServiceXMLService()
+//		ServiceContainerDTO serviceContainer = serviceXMLService.loadServiceData()
+//		serviceContainer.serviceDTOList.each {s ->
+//			println s
+//		}
+		
+//		TripXMLService tripXMLService = new TripXMLService()
+//		List<TripDTO> tripList = tripXMLService.loadTripData()
 //		tripList.each {trip ->
 //			println trip
 //		}
 
-		TripListService tripListService = new TripListService()
-		tripListService.fixMissingEndOdometer(tripList, travelList)
+//		TripListService tripListService = new TripListService()
+//		tripListService.fixMissingEndOdometer(tripList, travelList)
 //		tripList.each {trip ->
 //			println trip
 //		}
 
-		TripReportService service = new TripReportService()
-		service.execute(tripList, fuelList, travelList)
+//		TripReportService service = new TripReportService()
+//		service.execute(tripList, travelList)
 
 	}
 }
